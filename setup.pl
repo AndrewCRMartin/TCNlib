cc1e729a29b49ed97e72ddb803be2c3c9c2565cb
AndrewCRMartin <andrew@bioinf.org.uk>
2017-08-14 22:37:49 +0100

    Added Perl parameter to setup.pl

--- setup.pl
+++ setup.pl
@@ -1,15 +1,17 @@
-#!/usr/bin/env perl
+#!/usr/bin/perl -s
 use strict;
 use warnings;
 
-print "Initialising sub-modules ...\n";
-system("git submodule update --init --recursive");
+$::perl = 'perl' if(!defined($::perl));
+
+#print "Initialising sub-modules ...\n";
+#system("git submodule update --init --recursive");
 
 print "Getting external packages ...\n";
 system("./getexternalpackages");
 
 print "Getting perl dependencies ...\n";
-system("./getperldeps.pl");
+system("yes | $::perl ./getperldeps.pl");
 
 print "Copying TCNPerlVars.defaults to TCNPerlVars.pm ...\n";
 system("cp lib/TCNPerlVars.defaults lib/TCNPerlVars.pm");
