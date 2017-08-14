a389175b1dbcdddcea0850c651ab9f6ec8646cc2
AndrewCRMartin <andrew@bioinf.org.uk>
2017-08-14 16:48:33 +0100

    Temporary fix: lib/TCNUtil/FOSTA/Remote.pm is limited to downloading 10 sequences

---
+++ lib/TCNUtil/FOSTA/README.md
@@ -0,0 +1,8 @@
+Remote.pm
+=========
+
+By: ACRM
+--------
+
+**Note:** This version of `Remote.pm` is limited to download only 10 sequences.
+
--- lib/TCNUtil/FOSTA/Remote_original.pm
+++ lib/TCNUtil/FOSTA/Remote_original.pm
@@ -7,25 +7,13 @@ use TCNUtil::sequence;
 
 with 'FOSTA::FEPFinder';
 
-$::FEPLimit = 10;
-
 sub getReliableFEPSequencesFromSwissProtID {
     my $self        = shift;
     my $swissProtID = shift;
     my ($errorCode, @fepIDs) = WEB::FOSTA::GetFOSTA($swissProtID);
     croak "Error code $errorCode returned by WEB::FOSTA::GetFOSTA!"
         if $errorCode;
-
-    my @fepFASTAs;
-    my $count = 0;
-    foreach my $fepID (@fepIDs)
-    {
-        print "INFO: Getting sequence for $fepID\n";
-        push @fepFASTAs, UNIPROT::GetFASTA($fepID, -remote => 1);
-        last if($::FEPLimit && (++$count >= $::FEPLimit));
-    }
-#        @fepFASTAs = map {UNIPROT::GetFASTA($_, -remote => 1)} @fepIDs;
-
+    my @fepFASTAs = map {UNIPROT::GetFASTA($_, -remote => 1)} @fepIDs;
     return map {sequence->new($_)} @fepFASTAs;
 }
 
